003e568cdbf0c14b515b45bdcf29409a0d559b9c
little-zeno <fiery-chariot@hotmail.com>
2022-09-06 06:24:09 +0000

    Build a Pokemon Sprites Page to display all Pokemon - Utilize DOM manipulation to build a Pokemon Sprites Page to display all Pokemon.

---
+++ app.js
@@ -0,0 +1,16 @@
+const container = document.querySelector('#container');
+const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+
+for (let i=1; i <= 898; i++){
+    const pokemon = document.createElement('div');
+    pokemon.classList.add('pokemon')
+    const newImg = document.createElement('img');
+    newImg.src = `${baseURL}${i}.png`;
+    const label = document.createElement('span');
+    label.innerText = `#${i}`;
+
+    container.appendChild(pokemon);
+    pokemon.appendChild(newImg);
+    pokemon.appendChild(label);
+    
+}
